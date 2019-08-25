95f26725dbfaa6425419ce7730be326c8774b5e1
santoshverma8710 <50474494+santoshverma8710@users.noreply.github.com>
2019-08-25 16:13:37 +0530

    hello world 2

--- src/main/java/com/demo/springweb2/springWeb2/DemoApplication.java
+++ src/main/java/com/demo/springweb2/springWeb2/DemoApplication.java
@@ -2,7 +2,10 @@ package com.demo.springweb2.springWeb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +13,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@RequestMapping("/hello")
+	public String hello()
+	{
+		return "Hello World 2";
+	}
 }
